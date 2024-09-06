c913ab668faf787c45610feeda1144fbff4060c1
陈钰浩 <chenyh4@g-bits.com>
2024-09-06 14:31:45 +0800

    feat: 在asp.net core中依赖注册方法

---
+++ EFCoreDemo1/WebApiDotnet6/Controllers/InjectTestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiDotnet6.Services;
+
+namespace WebApiDotnet6.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class InjectTestController : ControllerBase
+{
+    // The CalculationService is injected into the controller
+    private CalculationService CalculationService { get; init; }
+
+    // The CalculationService is injected into the controller
+    public InjectTestController(CalculationService service)
+    {
+        CalculationService = service;
+    }
+
+    [HttpGet]
+    // This is a simple controller action that adds two numbers
+    public int Add(int a, int b) => CalculationService.Add(a, b);
+
+
+    [HttpGet]
+    // [FromServices] is used to inject a service into a controller action
+    public int Add2([FromServices] CalculationService calculationService, int a, int b)
+        => calculationService.Add(a, b);
+}
---
+++ EFCoreDemo1/WebApiDotnet6/Services/CalculationService.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApiDotnet6.Services;
+
+public class CalculationService
+{
+    public int Add(int x, int y) => x + y;
+}
