1d380bcf3860a16afd79562e117e69eaee5cfd88
陈钰浩 <chenyh4@g-bits.com>
2024-09-06 15:10:17 +0800

    feat: 服务器“建议”客户端缓存时长 [ResponseCache(Duration = 5)]

---
+++ ReflectionDemo/ReflectionDemo/Controllers/ResponseCacheController.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReflectionDemo.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class ResponseCacheController
+{
+    // GET: api/ResponseCache/GetDate
+    // 带缓存的接口，缓存时间为5秒
+    [HttpGet]
+    [ResponseCache(Duration = 5)]
+    public string GetDate()
+    {
+        return DateTime.Now.ToString(CultureInfo.InvariantCulture);
+    }
+}
